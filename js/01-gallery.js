2145f0596ec0ad2ff08796a0882a8f69eed641a5
ROPAN1991 <91415292+ROPAN1991@users.noreply.github.com>
2022-07-16 15:40:56 +0300

    hw07

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,35 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const gallery = document.querySelector(".gallery");
+const template = ({
+  preview,
+  original,
+  description,
+}) => `<div class="gallery__item">
+<a class="gallery__link" href="${original}">
+    <img
+    class="gallery__image"
+    src="${preview}"
+    data-source="${original}"
+    alt="${description}"
+  />
+</a>
+</div>`;
+const render = (array) => {
+  const items = array.reduce((acc, item) => (acc += template(item)), "");
+  gallery.innerHTML = items;
+};
+
+render(galleryItems);
+
+const links = Array.from(document.querySelectorAll(".gallery__link"));
+links.forEach((link) => {
+  link.addEventListener("click", (e) => {
+    e.preventDefault();
+    const item = galleryItems.find(
+      (el) => el.original === e.target.getAttribute("data-source")
+    );
+    basicLightbox.create(template(item)).show();
+  });
+});
