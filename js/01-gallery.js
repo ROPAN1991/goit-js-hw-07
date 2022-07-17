7e86c3ebe88e031a4522388d4a4349161786ef35
ROPAN1991 <91415292+ROPAN1991@users.noreply.github.com>
2022-07-17 18:35:07 +0300

    hw

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,6 +2,7 @@ import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 const gallery = document.querySelector(".gallery");
+
 const template = ({
   preview,
   original,
@@ -16,45 +17,36 @@ const template = ({
   />
 </a>
 </div>`;
+
 const render = (array) => {
-  const items = array.reduce((acc, item) => (acc += template(item)), "");
-  gallery.innerHTML = items;
+    const items = array.reduce((acc, item) => (acc += template(item)), '');
+    gallery.innerHTML = items;
 };
 
 render(galleryItems);
 
+gallery.addEventListener('click', e => {
+    const item = galleryItems.find(el => el.original === e.target.getAttribute('data-source'))
+
+    const instance = basicLightbox.create(template(item), {
+        onShow: () => {
+            document.addEventListener('keydown', closeByEscape)
+        },
+        onClose: () => {
+            document.removeEventListener('keydown', closeByEscape)
+        }
+    })
+
+    function closeByEscape(e) {
+        if (e.key === 'Escape') {
+            console.log('|eee')
+            instance.close()
+        }
+    }
+
+    instance.show()
+})
+
 const links = Array.from(document.querySelectorAll(".gallery__link"));
-links.forEach((link) => {
-  link.addEventListener("click", (e) => {
-    e.preventDefault();
-    const item = galleryItems.find(
-      (el) => el.original === e.target.getAttribute("data-source")
-    );
-    basicLightbox.create(template(item)).show();
-  });
-});
 
-// --------------------------------код подругі
-
-// const gallery = document.querySelector(".gallery");
-
-// const imagesSet = createImagesMarkup(galleryItems);
-
-// gallery.insertAdjacentHTML("beforeend", imagesSet);
-
-// function createImagesMarkup(items) {
-//   return items
-//     .map(({ preview, original, description }) => {
-//       return `<div class="gallery__item">
-//     <a class="gallery__link" href="${original}">
-//       <img
-//         class="gallery__image"
-//         src="${preview}"
-//         data-source="${original}"
-//         alt="${description}"
-//       />
-//     </a>
-//   </div>`;
-//     })
-//     .join("");
-// }
+links.forEach((link) => link.addEventListener('click', (e) => e.preventDefault()));
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,23 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const gallery = document.querySelector(".gallery");
+
+const template = ({
+  preview,
+  original,
+  description
+}) => `<a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+</a>`;
+
+const render = (array) => {
+  const items = array.reduce((acc, item) => (acc += template(item)), '');
+  gallery.innerHTML = items;
+};
+
+render(galleryItems);
+
+new SimpleLightbox('.gallery a', {
+  captionDelay: 100
+})
